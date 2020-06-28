8f061f2f61918795fab114a7503ecf238396c66f
Michael Spengler <mycamper@gmx.de>
2020-06-29 01:04:04 +0200

    improving the data model

--- cityservice.ts
+++ cityservice.ts
@@ -1,7 +1,8 @@
 // import { cities } from './cities.ts'
 import { DistanceCalculator } from "https://deno.land/x/distancecalculator/distance-calculator.ts"
-import { cities } from 'https://raw.githubusercontent.com/michael-spengler/cities/master/cities.ts'
-// import { cities } from './cities.ts'
+// import { cities } from 'https://raw.githubusercontent.com/michael-spengler/cities/master/cities.ts'
+import { cities } from './cities.ts'
+import { ICityInfo } from "./interfaces.ts"
 // import { cities } from 'https://deno.land/x/cities/cities.ts'
 
 
@@ -9,7 +10,7 @@ export class CityService {
 
     private static minimumPopulation = 40000
 
-    public static getCityInfo(countryCode: string, cityName: string): any {
+    public static getCityInfo(countryCode: string, cityName: string): ICityInfo {
         return cities.filter((city) => city.country === countryCode && city.name === cityName)[0]
     }
 
@@ -17,7 +18,7 @@ export class CityService {
         let cityWithLowestDistance
         let lowestDistance = 10000000000
         for (const city of cities.filter((entry: any) => entry.population >= minimumPopulation)){
-            const distance = DistanceCalculator.getDistanceInKilometers(latitude, longitude, city.loc.coordinates[1], city.loc.coordinates[0])
+            const distance = DistanceCalculator.getDistanceInKilometers(latitude, longitude, city.lat, city.lon)
             if (distance < lowestDistance) {
                 lowestDistance = distance
                 cityWithLowestDistance = city
---
+++ interfaces.ts
@@ -0,0 +1,12 @@
+export interface ICityInfo {
+    cityId: number,
+    name: string,
+    altName: string,
+    country: string,
+    featureCode: string,
+    adminCode: string,
+    population: number,
+    lat: number,
+    lon: number,
+}
+
